410c84cef791c3de6dc0941c84a2a2f90980bf1d
Tokioshi <grizzlizyping@gmail.com>
2026-06-26 23:44:25 +0700

    I have to use dns since I got blocked from connecting to the database

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import { ExtendedClient } from "./structures/Client.js";
 import { env } from "./config/env.js";
 import { loadHandlers } from "./handler/index.js";
+import dns from "node:dns";
+
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
 const client = new ExtendedClient();
 
