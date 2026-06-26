7e269f8f0cec56b8fedb67316245af6fda304db7
Tokioshi <grizzlizyping@gmail.com>
2026-06-26 23:43:56 +0700

    Conneect to the mongodb database

--- src/events/clientReady.ts
+++ src/events/clientReady.ts
@@ -1,17 +1,19 @@
-import {
-    ActionRowBuilder,
-    ButtonBuilder,
-    ButtonStyle,
-    EmbedBuilder,
-    Events,
-    TextChannel,
-} from "discord.js";
+import { Events } from "discord.js";
+import { env } from "../config/env.js";
 import type { Event } from "../structures/Event.js";
+import mongoose from "mongoose";
 
 export default {
     name: Events.ClientReady,
     once: true,
     execute(client) {
-        console.log(`[Client] Logged in as ${client.user.tag}`);
+        console.log(`[Client] Logged in as ${client.user.tag}`!);
+
+        try {
+            mongoose.connect(env.mongoURL);
+            console.log("[Database] MongoDB connected successfully!")
+        } catch (error) {
+            console.error("Faild connecting to the database:", error);
+        }
     },
 } satisfies Event<Events.ClientReady>;
